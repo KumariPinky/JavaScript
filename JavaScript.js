41ec64b8ff0592511e632f4d1ef822a8f894ce66
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-22 23:10:19 +0530

    this is javascript program

---
+++ JavaScript.js
@@ -0,0 +1,44 @@
+// 1. Program to print text
+
+// console.log("Hello this is First javaScript program")
+
+// 2. Program To Read Two Numbers And Print The Sum Of Given Two Numbers.
+// var a =20;
+// var b=25;
+// console.log("sum of two number:", a+b);
+
+// 3. Program To Accept Student Roll No, Marks in 3 Subjects and Calculate Total, Average and Print it.
+
+// let rollno = prompt("Enter student Rollno:")
+// let eng=89;
+// let math = 90;
+// let science =87;
+// let total = eng+math+science;
+// let avg = total/3;
+// console.log(total);
+// console.log(avg)
+
+
+// 4. Program To Read Three Numbers And Print The Biggest Of Given Three Numbers
+// var big=0;
+// var x=30;
+// var y=5;
+// var z=20;
+// if(x>big){
+//     big=x; 
+// }
+//  if(y>big){
+//     big=y;
+// }
+//  if(z>big){
+//     big=z;    
+// }
+// console.log(big);
+
+// . Program To Read A Number And Find Whether The Given Number Is Even Or Odd.
+var num =12;
+if(num%2==0){
+    console.log("Even Number");
+}else{
+    console.log("Odd")
+}
--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
 console.log("Hello javascript");
+
+let a =29;
+let b=20;
+console.log(a+b)
