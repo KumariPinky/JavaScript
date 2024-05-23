d9245ab15de373ed14400dd71b6c00f777a4f29a
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-23 18:30:39 +0530

    Leap Year

---
+++ Program6.js
@@ -0,0 +1,8 @@
+// 6. Program to accept a year and check whether the given year IS leap year or not.
+var y =2023;
+if(y%4==0&&y%100!=0|| y%400==0){
+    console.log("leap year")
+}else{
+    console.log("not leap year")
+}
+
