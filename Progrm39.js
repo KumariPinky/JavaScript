49b70b682d322ab1bfef7ce17c28aa23878b1a87
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-24 18:32:23 +0530

    five numbers in reverse order

---
+++ Progrm39.js
@@ -0,0 +1,10 @@
+// 39. Program to accept 10 numbers and print first five numbers in original order and print last five numbers in reverse order
+
+
+    for(var j =1;j<=5;j++){
+        console.log(j);
+    }
+    for(var k = 10;k>=6;k--){
+     console.log(k);
+    }
+    ;
