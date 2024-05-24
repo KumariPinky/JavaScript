30d55c2cf587bd6011662f943d55b38ae38a90f2
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-24 18:06:44 +0530

    factorial of given number

---
+++ Program35.js
@@ -0,0 +1,9 @@
+// 35. Program to accept two numbers and print sum of two numbers by using functions
+
+let a= 15;
+let b =20;
+
+// let x= window.prompt("Enter any number");
+// let y= window.prompt("Enter any number");
+let sum = a+b;
+console.log(sum);
---
+++ Program36.js
@@ -0,0 +1,23 @@
+// 36. Program to accept a number and find factorial of given number
+
+var fact =1;
+let number=6;
+
+// if(number==0){
+//     console.log("The factorial of "+ number);
+// } else if(number<0){
+//     console.log("The factorial of -ve")
+// }else{
+//     for(var i=1;i<=number;i++){
+       
+//         fact =fact*i;
+//         console.log(fact)
+//     }
+// }
+
+if(number>0){
+    for(var i=1;i<number;i++){
+        fact =fact*i
+        console.log(fact0)
+    }
+}
