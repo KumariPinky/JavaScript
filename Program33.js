90701ca945ec7f58d502fed008c37b5959629e11
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-24 17:01:00 +0530

    capital letters, no. of small letters

---
+++ Program33.js
@@ -0,0 +1,49 @@
+// 33. Program to accept any single digit number and print it in words .
+
+
+// function printWord(N){
+//     var i;
+//    var length = N.length;
+//     for(i=0; i<length;i++){
+//         printValue(N[i]);
+//     }
+// }
+// var N ="1";
+// printWord();  
+
+var digit =2;
+switch(digit){
+        case 0:
+        console.log("zero");
+        break;
+        case 1:
+        console.log("one");
+        break;
+         case 2:
+        console.log("two");
+        break;
+         case 3:
+        console.log("three");
+        break;
+         case 4:
+        console.log("four");
+        break;
+         case 5:
+        console.log("five");
+        break;
+         case 6:
+        console.log("six");
+        break;
+         case 7:
+        console.log("seven");
+        break;
+         case 8:
+        console.log("eight");
+        break;
+         case 9:
+        console.log("nine");
+        break;
+    default :
+        console.log("invalid")
+    
+}
