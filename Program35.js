a94f61024eb3f6e851447213fdd0a0723eadbdea
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-24 18:09:47 +0530

    sum of two numbers by using functions

--- Program35.js
+++ Program35.js
@@ -1,9 +1,17 @@
 // 35. Program to accept two numbers and print sum of two numbers by using functions
 
-let a= 15;
-let b =20;
+// let a= 15;
+// let b =20;
+
+
+// let sum = a+b;
+// console.log(sum);
+
+function sum(a,b){
+    sum=a+b;
+    console.log(sum);
+}
+sum(2,4);
 
 // let x= window.prompt("Enter any number");
 // let y= window.prompt("Enter any number");
-let sum = a+b;
-console.log(sum);
