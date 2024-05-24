2f0af7d8866d1029bd3c30321fde07cdc74445a4
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-24 19:04:39 +0530

    reverse of the given string

---
+++ Program39.js
@@ -0,0 +1,15 @@
+// 40. Program to accept a string and print the reverse of the given string by using for loop.
+
+
+// let str=  "Pinky Kumari";
+//  for(var i=str.length;i<0;i--){
+//     console.log(str);
+//  }
+
+var str = "Pinky Mehta";
+var revStr="";
+for(var i =str.length-1;i>=0;i--){
+   revStr+=str[i]
+  
+}
+ console.log(revStr)
