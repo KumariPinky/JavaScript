4f8b8d33f5d9c42d52094768e892b77dfa13adb9
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-29 13:42:03 +0530

    prime number between 1-100

--- Program57.js
+++ Program57.js
@@ -8,3 +8,28 @@
 //      for(let i=0;i<max.length;i++){
 //         for(let j=0;j<)
 //      }
+
+// A=[[1,2,3],
+//    [4,5,6],
+//    [7,8,9]];
+
+// B=[[1,2,3],
+//    [3,4,5],
+//    [5,6,7]];
+
+//    result = math.multiply(A,B)
+//    result.forEach((n)=>console.log(n,'<BR>'));
+
+// const a[][]= new const [3][3];
+// const b[][]= new const [3][3];
+// const c[][]= new const [3][3];
+// var j,i;
+
+// for()
+
+
+
+
+
+
+
