d1c1da5dd064f41cdb1366af3149293d155b114c
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-05-30 15:17:09 +0530

    calculate total marks,

---
+++ Program61.js
@@ -0,0 +1,16 @@
+// 61. Program a structure which reads ‘n’students information (name,3 subjects marks)and 
+// calculate total marks, result print them in a particular format.
+
+
+var math=50;
+var eng = 70;
+var hindi=80;
+var sum=0;
+// let a = [50,70,80];
+// for(let i =0; i<=a.length;i++){
+//     sum=sum+i;
+// }
+// console.log(sum);
+
+sum = math+eng+hindi;
+console.log(sum);
