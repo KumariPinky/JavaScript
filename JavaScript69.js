c4d9ab37f60c70c5857e526b7c5df920a2c70001
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-06-04 13:51:28 +0530

    Right rotate an array by k position

---
+++ JavaScript69.js
@@ -0,0 +1,32 @@
+// Right rotate an array by k position.
+
+
+let arr = [1,2,3,4,5,6,7,8];
+let k= 4;
+
+function rotationClockWise(arr,k){
+    for(let i =0; i<k ; i++){
+        let element = arr.shift()
+        arr.push(element);
+    }
+ 
+}
+rotationClockWise(arr,k);
+console.log(arr);
+
+
+
+// rotationAntiClockWise
+
+// function rotationAntiClockWise(arr,k){
+//     for(let i =0; i<k ; i++){
+//         let element = arr.pop()
+//         arr.unshift(element);
+//     }
+ 
+// }
+// rotationAntiClockWise(arr,k);
+// console.log(arr);
+
+
+
