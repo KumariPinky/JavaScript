f0d65f58cc53e6a24018468b7e9c1994487498be
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-06-20 21:50:46 +0530

    max and min value in a given array

---
+++ js2.js
@@ -0,0 +1,7 @@
+// find max and min value in a given array in javascript;
+
+let arr =[12,14,16,18,19,30,29];
+let max =Math.max(...arr);
+console.log(max);
+let min = Math.min(...arr);
+console.log(min);
