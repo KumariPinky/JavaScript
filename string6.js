806d0021f7e1b95e22e251cb7d077626aa9a9a99
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-06-21 23:32:06 +0530

    go pageless by default

---
+++ string6.js
@@ -0,0 +1,10 @@
+//  Input :- Go Pageless By Default
+// Output - go pageless by default
+
+function lowercaseInput(input) {
+  return input.toLowerCase();
+}
+
+let input = "Go Pageless By Default";
+let output = lowercaseInput(input);
+console.log(output); // Output: "go pageless by default" 
