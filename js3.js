3a1a5a0990a8aade67a86771bcd8cf7d47059a5b
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-06-26 10:55:10 +0530

    javascript

--- js3.js
+++ js3.js
@@ -1,1 +1,8 @@
 console.log("hello");
+
+function returns(a,b){
+ return a+b;
+
+}
+
+console.log(returns(5,5));
--- js4.js
+++ js4.js
@@ -2,3 +2,9 @@ let a = "pinky";
 let b = 23;
 let sum = a+b;
 console.log(sum);
+
+console.log(a);
+
+for(let i =1;i<=5;i++){
+    console.log(i);
+}
