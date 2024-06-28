22c8fbc16c5dacfc8daeca7cfda46062f7bd62f8
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-06-28 07:13:11 +0530

    array concept

---
+++ array.js
@@ -0,0 +1,12 @@
+let arry = ["pinky", "rinky", "pooja", "nitesh"];
+
+for(let i=0; i<arry.length; i++){
+    console.log(arry[i]);
+}
+
+//while loop
+let i =0;
+while(i<arry.length){
+    console.log(arry[i]);
+    i++;
+}
