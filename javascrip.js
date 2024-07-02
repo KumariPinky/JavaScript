9f16040fea852842c553ae7b0aa1df78820aabce
KumariPinky <131809483+KumariPinky@users.noreply.github.com>
2024-07-02 08:53:16 +0530

    interview question

---
+++ javascrip.js
@@ -0,0 +1,18 @@
+// which of the following type of variable everywher in your javascript code?
+
+// ans  global variable
+
+
+Q // which build-in method reverses the order of the element of an array.
+
+ans   reverse()
+
+
+// which built-in method return the calling string value converted to lower case?
+
+ans  toLowerCase()
+
+// remove an attribute from each of the matched elements?
+
+ans  removeAttr()
+
